6aafacfa317cc377859cd596420a8705e0573a0b
eyasq <qeremeyas99@gmail.com>
2025-05-31 17:12:21 +0300

    it actually does something now

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self,width, height, title, bg="white" ):
+    def __init__(self,width, height ):
         self.width = width
         self.height = height
         self.__root = Tk()
-        self.__root.title = title
+        self.__root.title = "title"
         self.canvas = Canvas()
         self.canvas.pack()
         self.running = False
@@ -28,13 +28,18 @@ class Window:
         self.__root.protocol("WM_DELETE_WINDOW", self.close )
 
 
-def main(self):
-    self.win = Window(800, 600)
-    self.win.wait_for_close()
-    point1 = Point(20, 20)
+def main():
+    win = Window(800, 600)
+    point1 = Point(0, 0)
     point2 = Point(40,40)
+    point3 = Point(100, 0)
+    point4 = Point(50, 50)
     line = Line(point1, point2)
-    line.draw(self.win.canvas, "white")
+    line2 = Line(point3, point4)
+    line.draw(win.canvas, "black")
+    line2.draw(win.canvas, "magenta")
+    win.wait_for_close()
+
 
 class Point:
     def __init__(self, x, y):
@@ -49,7 +54,7 @@ class Line:
 
     def draw(self, Canvas, fill_color):
         Canvas.create_line(
-            self.point1.x, self.point1.y, self.point.x, self.point2.y, fill=fill_color, width=2
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
         )
     
 
