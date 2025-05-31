48ed42acd0cb7e8c04d7701522b016da56f88b6b
eyasq <qeremeyas99@gmail.com>
2025-06-01 02:10:55 +0300

    recursion inducded death

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 import time
 import random
+
 class Window:
-    def __init__(self,width, height ):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
         self.__root = Tk()
-        self.__root.title = "title"
+        self.__root.title = "Maze Solver"
         self.canvas = Canvas(self.__root, width=self.width, height=self.height)
         self.canvas.pack()
         self.running = False
@@ -17,22 +18,21 @@ class Window:
     
     def wait_for_close(self):
         self.running = True
-        while(self.running == True):
+        while self.running == True:
             self.redraw()
     
     def draw_line(self, line, fill_color):
         line.draw(self.canvas, fill_color)
         
-
     def close(self):
         self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close )
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
 
 def main():
     win = Window(1280, 960)
     point1 = Point(0, 0)
-    point2 = Point(40,40)
+    point2 = Point(40, 40)
     point3 = Point(100, 0)
     point4 = Point(50, 50)
     line = Line(point1, point2)
@@ -56,12 +56,12 @@ def main():
     win.wait_for_close()
 
 
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        #x=0 is left, y=0 is top
+        # x=0 is left, y=0 is top
+
 
 class Line:
     def __init__(self, point1, point2):
@@ -72,7 +72,8 @@ class Line:
         Canvas.create_line(
             self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
         )
-    
+
+
 class Cell:
     def __init__(self, window=None):
         self.has_left_wall = True
@@ -85,57 +86,74 @@ class Cell:
         self.__y2 = -1
         self.__win = window
         self.visited = False
-    def draw(self,x1,y1,x2,y2):
+
+    def draw(self, x1, y1, x2, y2):
         self.__x1 = x1
         self.__x2 = x2
         self.__y1 = y1
         self.__y2 = y2
+        
         if self.has_left_wall:
-            point1 = Point(x1,y1)
+            point1 = Point(x1, y1)
             point2 = Point(x1, y2)
             line = Line(point1, point2)
             if self.__win:
                 line.draw(self.__win.canvas, "black")
         else:
-            if self.win:
+            point1 = Point(x1, y1)
+            point2 = Point(x1, y2)
+            line = Line(point1, point2)
+            if self.__win:
                 line.draw(self.__win.canvas, "white")
+                
         if self.has_right_wall:
-            point1 = Point(x2,y1)
-            point2 = Point(x2,y2)
-            line  = Line(point1,point2)
+            point1 = Point(x2, y1)
+            point2 = Point(x2, y2)
+            line = Line(point1, point2)
             if self.__win:
                 line.draw(self.__win.canvas, "black")
         else:
-            if self.win:
+            point1 = Point(x2, y1)
+            point2 = Point(x2, y2)
+            line = Line(point1, point2)
+            if self.__win:
                 line.draw(self.__win.canvas, "white")
+                
         if self.has_top_wall:
-            point1 = Point(x1,y1)
-            point2 = Point(x2,y1)
-            line  = Line(point1,point2)
+            point1 = Point(x1, y1)
+            point2 = Point(x2, y1)
+            line = Line(point1, point2)
             if self.__win:
                 line.draw(self.__win.canvas, "black")
         else:
-            if self.win:
+            point1 = Point(x1, y1)
+            point2 = Point(x2, y1)
+            line = Line(point1, point2)
+            if self.__win:
                 line.draw(self.__win.canvas, "white")
+                
         if self.has_bottom_wall:
-            point1 = Point(x1,y2)
-            point2 = Point(x2,y2)
-            line  = Line(point1,point2)
+            point1 = Point(x1, y2)
+            point2 = Point(x2, y2)
+            line = Line(point1, point2)
             if self.__win:
                 line.draw(self.__win.canvas, "black")
         else:
-            if self.win:
+            point1 = Point(x1, y2)
+            point2 = Point(x2, y2)
+            line = Line(point1, point2)
+            if self.__win:
                 line.draw(self.__win.canvas, "white")
         
     def draw_move(self, to_cell, undo=False):
-        self.center_x = (self.__x1 + self.__x2)/2
-        self.center_y = (self.__y1+self.__y2)/2
-        to_cell.center_x = (to_cell.__x1+to_cell.__x2)/2
-        to_cell.center_y = (to_cell.__y1+to_cell.__y2)/2
+        self.center_x = (self.__x1 + self.__x2) / 2
+        self.center_y = (self.__y1 + self.__y2) / 2
+        to_cell.center_x = (to_cell.__x1 + to_cell.__x2) / 2
+        to_cell.center_y = (to_cell.__y1 + to_cell.__y2) / 2
         center_self = Point(self.center_x, self.center_y)
         center_to = Point(to_cell.center_x, to_cell.center_y)
         line = Line(center_self, center_to)
-        if undo==False:
+        if undo == False:
             if self.__win:
                 line.draw(self.__win.canvas, "red")
         else:
@@ -144,7 +162,7 @@ class Cell:
 
 
 class Maze:
-    def __init__(self, x1 ,y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -157,15 +175,24 @@ class Maze:
             random.seed(seed)
         self.__create_cells()
         self.__break_entrance_and_exit()
+        if self.win:  # Only generate maze if we have a window (for visual display)
+            self.__break_walls_r(0, 0)
+            self.__reset_cells_visited()
+
     def __create_cells(self):
         for i in range(self.num_cols):
             self.__cells.append([])
             for j in range(self.num_rows):
                 self.__cells[i].append(Cell(self.win))
                 if self.win:
-                    self.__draw_cell(i,j)
+                    self.__draw_cell(i, j)
         
-    def __draw_cell(self,i,j):
+    def __draw_cell(self, i, j):
+        if i == -1:
+            i = self.num_cols - 1
+        if j == -1:
+            j = self.num_rows - 1
+            
         x1 = self.x1 + (i * self.cell_size_x)  
         y1 = self.y1 + (j * self.cell_size_y)  
         x2 = x1 + self.cell_size_x
@@ -179,11 +206,21 @@ class Maze:
             self.win.redraw()
 
     def __break_entrance_and_exit(self):
+        # Break entrance (top-left cell) - remove top wall only
         self.__cells[0][0].has_top_wall = False
-        self.__draw_cell(0,0)
-        self.__cells[-1][-1].has_bottom_wall = False
-        self.__draw_cell(-1,-1)
+        if self.win:
+            self.__draw_cell(0, 0)
     
+        # Break exit (bottom-right cell) - remove bottom wall only
+        self.__cells[self.num_cols - 1][self.num_rows - 1].has_bottom_wall = False
+        if self.win:
+            self.__draw_cell(self.num_cols - 1, self.num_rows - 1)
+
+    def __reset_cells_visited(self):
+        for col in self.__cells:
+            for cell in col:
+                cell.visited = False
+
     def __break_walls_r(self, i, j):
         self.__cells[i][j].visited = True
         
@@ -208,7 +245,8 @@ class Maze:
             
             # If no directions to go, draw cell and return
             if len(to_visit) == 0:
-                self.__draw_cell(i, j)
+                if self.win:
+                    self.__draw_cell(i, j)
                 return
             
             # Randomly pick a direction to go
@@ -228,4 +266,3 @@ class Maze:
                 self.__cells[next_i][next_j].has_top_wall = False
             
             self.__break_walls_r(next_i, next_j)
-
--- src/tests.py
+++ src/tests.py
@@ -35,6 +35,37 @@ class Tests(unittest.TestCase):
         
         self.assertTrue(m._Maze__cells[0][0].has_bottom_wall)
         self.assertTrue(m._Maze__cells[-1][-1].has_top_wall)
+    
+    def test_break_entrance_and_exit(self):
+        num_cols = 5
+        num_rows = 5
+        m = Maze(0, 0, num_rows, num_cols, 10, 10)
+    
+    # Test entrance (top-left cell)
+        self.assertFalse(m._Maze__cells[0][0].has_top_wall)  # Entrance top wall removed
+    
+    # Verify entrance other walls still exist
+        self.assertTrue(m._Maze__cells[0][0].has_left_wall)
+        self.assertTrue(m._Maze__cells[0][0].has_right_wall)
+        self.assertTrue(m._Maze__cells[0][0].has_bottom_wall)
+    
+    # Test exit (bottom-right cell)
+        self.assertFalse(m._Maze__cells[-1][-1].has_bottom_wall)  # Exit bottom wall removed
+    
+    # Verify exit other walls still exist
+        self.assertTrue(m._Maze__cells[-1][-1].has_left_wall)
+        self.assertTrue(m._Maze__cells[-1][-1].has_right_wall)
+        self.assertTrue(m._Maze__cells[-1][-1].has_top_wall)  # This was failing before
+
+    # Verify no other cells were modified
+        for i in range(num_cols):
+            for j in range(num_rows):
+                if (i, j) not in [(0, 0), (num_cols-1, num_rows-1)]:
+                    cell = m._Maze__cells[i][j]
+                    self.assertTrue(cell.has_top_wall)
+                    self.assertTrue(cell.has_bottom_wall)
+                    self.assertTrue(cell.has_left_wall)
+                    self.assertTrue(cell.has_right_wall)
 
 
 if __name__ == "__main__":
